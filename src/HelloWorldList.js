6843be5e6daa6d5d1524eae8671356158d229cc1
ddosque <dpbergquist@gmail.com>
2017-08-01 18:17:38 +0100

    add greeter component

---
+++ src/HelloWorldList.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import HelloWorld from './HelloWorld';
+import AddGreeter from './AddGreeter';
+import './HelloWorldList.css';
+
+class HelloWorldList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { greetings: ['Jim', 'Sally', 'Bender'] };
+    this.addGreeting = this.addGreeting.bind(this);
+  }
+
+  addGreeting(newName) {
+    this.setState({ greetings: [...this.state.greetings, newName] });
+  }
+
+  renderGreetings() {
+    return this.state.greetings.map(name => (
+      <HelloWorld key={name} name={name}/>
+    ));
+  }
+
+  render() {
+    return (
+      <div className="HelloWorldList">
+        <AddGreeter addGreeting={this.addGreeting}/>
+        {this.renderGreetings()}
+      </div>
+    )
+  }
+}
+
+export default HelloWorldList;
